732642e073b2692623b5c03d15da16ac5b4c35ad
JamesKnight0001 <80499782+JamesKnight0001@users.noreply.github.com>
2023-02-06 20:02:28 -0800

    Create Update.lua

---
+++ Update.lua
@@ -0,0 +1,13 @@
+Library:UpdatesMain({
+	name = "base", -- really noy needed
+	image = "rbxassetid://12405540485",
+	update = "NEW AI MOVEMENT",
+	comment = "this is ez"
+})
+
+Library:UpdatesMain({
+	name = "base", -- really noy needed
+	image = "8964489645",
+	update = "Your mom",
+	comment = "lol"
+})
