ec42954b09b184355d41b2a89bbc90ccabcc70de
JamesKnight0001 <80499782+JamesKnight0001@users.noreply.github.com>
2023-02-06 20:12:36 -0800

    Update Update.lua

--- Update.lua
+++ Update.lua
@@ -1,13 +1,3 @@
-Library:UpdatesMain({
-	name = "base", -- really noy needed
-	image = "rbxassetid://12405540485",
-	update = "NEW AI MOVEMENT",
-	comment = "this is ez"
-})
+Library:UpdatesMain()
 
-Library:UpdatesMain({
-	name = "base", -- really noy needed
-	image = "http://www.roblox.com/asset/?id=8964489619",
-	update = "Your mom",
-	comment = "lol"
-})
+Library:UpdatesMain()
